520b252c4f78c460e84c10e6d41d5d5d2ef80b70
Courtney Adesile <ca@largeinc.com>
2026-06-03 21:55:59 +0100

    chore: replace wunderkindfactory.com with buildmyclub.co.uk throughout

--- src/Controller/Api/BetaRequestController.php
+++ src/Controller/Api/BetaRequestController.php
@@ -53,7 +53,7 @@ class BetaRequestController extends AbstractController
         }
 
         $message = (new Email())
-            ->from('noreply@wunderkindfactory.com')
+            ->from('noreply@buildmyclub.co.uk')
             ->to($recipientEmail)
             ->subject('Beta Access Request — ' . $email)
             ->text(implode("\n", [
